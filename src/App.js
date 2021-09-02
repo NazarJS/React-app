748f24a2add09510c8ed87903fd1f435a8c96c7a
NazarJS <NPaluyanau@gomel.iba.by>
2021-09-02 12:10:49 +0300

    new changes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component, useState } from "react";
+import "./App.css";
+import Header from "./Header/Header";
+import Input from "./Input/Input";
+import LattestNewsList from "./LattestNewsList/LatestNewsList";
+import TopNews from "./TopNews/TopNews";
+import Componentzxc from "./Componentzxc";
+import { reject } from "q";
+ 
+function App () {
+  
+   return (
+   <div className = 'App'>
+     <h1></h1>
+    
+    
+    <Header></Header>
+      <div className='Container'>
+        <Input/>
+        <TopNews/>
+        <LattestNewsList />
+      </div>
+    
+    
+   </div>)
+  }
+
+
+export default App;
